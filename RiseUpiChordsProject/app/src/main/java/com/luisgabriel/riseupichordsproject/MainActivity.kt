ab2bd7b0531cf3fab3d9ff441adf80e4e582d2ee
Luis Gabriel Correia <luisgabrieltech@gmail.com>
2023-11-15 01:57:29 -0300

    - Nav bar ( funcional ) ( troca de telas ) - ViewBinding - enable

--- RiseUpiChordsProject/app/src/main/java/com/luisgabriel/riseupichordsproject/MainActivity.kt
+++ RiseUpiChordsProject/app/src/main/java/com/luisgabriel/riseupichordsproject/MainActivity.kt
@@ -2,10 +2,28 @@ package com.luisgabriel.riseupichordsproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.luisgabriel.riseupichordsproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
     }
+
+    private fun initNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.btNavView, navController)
+    }
+
 }
