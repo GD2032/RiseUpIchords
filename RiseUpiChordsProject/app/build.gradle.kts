5c1e45c661df8bd30e2767c7dd70f65148f28e83
Luis Gabriel Correia <luisgabrieltech@gmail.com>
2023-11-14 21:31:55 -0300

    Updates 1.0
    - Gradle version ( 33 -> 34 )
    - Color pallet

--- RiseUpiChordsProject/app/build.gradle.kts
+++ RiseUpiChordsProject/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.luisgabriel.riseupichordsproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.luisgabriel.riseupichordsproject"
